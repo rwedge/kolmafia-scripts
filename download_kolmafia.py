f72ca4cc77fe6e3cf4fe7d5e3ad84a5245dcf5c6
rwedge <roy.wedge@gmail.com>
2018-12-26 19:03:01 -0600

    add script to download last successful kolmafia build

---
+++ download_kolmafia.py
@@ -0,0 +1,24 @@
+import io
+import os
+import re
+import zipfile
+
+import requests
+
+EXE_NAME = "kolmafia.jar"
+PWD = os.path.dirname(os.path.abspath(__file__))
+ZIP_LINK = "https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/*zip*/archive.zip"
+
+if __name__ == "__main__":
+    r = requests.get(ZIP_LINK)
+    with io.BytesIO(r.content) as f:
+        with zipfile.ZipFile(f) as z:
+            files = z.namelist()
+            for filename in files:
+                if re.match(r'archive/dist/KoLmafa-[0-9]*\.jar', filename) is not None:
+                    break
+            with z.open(filename, 'r') as jar:
+                jar_bytes = jar.read()
+
+    with io.open(EXE_NAME, 'wb') as jar:
+        jar.write(jar_bytes)
